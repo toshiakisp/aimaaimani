917b07b5debf8f9c5eaf5b86117b94eda6fad562
toshiakisp <toshiaki.sp@mail>
2014-09-15 12:00:46 +0900

    Improve build.sh to make xpi with revision sign

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
-cd aima_aimani
-zip -q -r -9 ../aima_aimani.xpi *
+
+GITREV=$(git log -1 --pretty=format:"git%ad.%h" --date=short|sed s/-//g)
+
+rm -rf build
+cp -r aima_aimani build
+cd build
+
+sed -e "s/\(<\/em:version>\)/.${GITREV}\1/" install.rdf > install.rdf.new && mv install.rdf.new install.rdf
+sed -e "s/\(Aima_AimaniVersion = \"[^\"]*\)/\1.${GITREV}/" chrome/content/version.js > chrome/content/version.js.new && mv chrome/content/version.js.new chrome/content/version.js
+
+zip -q -r -9 ../aima_aimani-$GITREV.xpi *
+cd ..
+
+rm -rf build/
 
