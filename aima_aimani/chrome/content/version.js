6714181dbbe7323587df4eb77082a756bdfc732f
toshiakisp <toshiaki.sp@mail>
2016-11-15 00:18:33 +0900

    Version sp_rev.3

--- aima_aimani/chrome/content/version.js
+++ aima_aimani/chrome/content/version.js
@@ -1,3 +1,3 @@
 /* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 
-var Aima_AimaniVersion = "3.3.57.sp_rev.2"; /* String  合間合間にのバージョン */
+var Aima_AimaniVersion = "3.3.57.sp_rev.3"; /* String  合間合間にのバージョン */
